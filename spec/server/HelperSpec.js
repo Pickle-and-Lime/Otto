e986ca346322c2c98730f6136ee13562069662cb
Jonathan Mason <jmason.p@gmail.com>
2015-09-07 13:14:03 -0500

    (feat) Refactors tests to use promises. Minor refactor in list-helpers. Closes #47.
    Completes refactor using promises and adds test for auto build list function.
    
    Removes unneeded normalization. Refactors add to list to save multiple times.

--- spec/server/HelperSpec.js
+++ spec/server/HelperSpec.js
@@ -29,142 +29,126 @@ var test = redtape({
   }
 });
 
-test('addToPantry() should add an item to the household\'s pantry', function(t){  
-  
-  setTimeout(function(){
-    addToPantry('milk', household1._id, 7, 30);
-  }, 3000);
+//Correct names of data
+//Remove /365 from list-helpers
+//
 
-  setTimeout(function(){
-    Household.findOne({_id: household1._id }, 'pantry', function(err, household){
+test('addToPantry() should add an item to the household\'s pantry', function(t){  
+  addToPantry('Milk', household1._id, 7, 30) // tracked with date specified
+  .then(function() {
+    return addToPantry('Rice', household1._id); // untracked without date specified
+  })
+  .then(function() {
+    return Household.findOne({ _id: household1._id }, 'pantry', function(err, household){
       if (err) { t.error(err); }
-      t.ok("milk" in household.pantry, 'Pantry created!');
+      t.ok('Milk' in household.pantry, 'Milk added to pantry!');
+      t.ok('Rice' in household.pantry, 'Rice added to created!');
+      t.end();
     });
-  }, 5000);
-  
-  t.end();
+  });
 });
 
-/*test('addToList() should add any item to the household\'s list', function(t){
-  setTimeout(function(){
-    addToList('fruit', household1._id);
-  }, 7000);
-
-  setTimeout(function(){
-    addToList('chicken', household1._id);
-  }, 10000);
-
-  setTimeout(function(){
-    Household.findOne({_id: household1._id }, 'pantry list', function(err, household){
-      //console.log(household);
-      t.ok('fruit' in household.list, 'Fruit added to list.');
-      t.ok('chicken' in household.list, 'Chicken added to list.');
+test('addToList() should add any item to the household\'s list', function(t){
+  
+  addToList('Berries', household1._id)
+  .then(function(){
+    addToList('Chicken', household1._id);
+  })
+  .then(function(){
+    Household.findOne({ _id: household1._id }, 'pantry list', function(household) {
+      t.ok('Berries' in household.list, 'Berries added to list.');
+      t.ok('Chicken' in household.list, 'Chicken added to list.');
+      t.end();
     });
-  }, 18000);
-
-  t.end();
+  });
 });
 
 test('removeFromList() should remove the passed in item from the household\'s list', function(t){
-  setTimeout(function(){
-    removeFromList('fruit', household1._id);
-  }, 15000);
-
-  setTimeout(function(){
-    removeFromList('chicken', household1._id);
-  }, 18000);
-
-  setTimeout(function(){
+  
+  removeFromList('Berries', household1._id)
+  .then(function(){
+    removeFromList('Chicken', household1._id);
+  })
+  .then(function(){
     Household.findOne({_id: household1._id }, 'pantry list', function(err, household){
-      
-      t.notOk('fruit' in household.list, 'Fruit removed from list.');
-      t.notOk('chicken' in household.list, 'Chicken removed from list.');
+      t.notOk('Berries' in household.list, 'Berries removed from list.');
+      t.notOk('Chicken' in household.list, 'Chicken removed from list.');
     });
-  }, 23000);
-
   t.end();
+  });
 });
 
 test('getPantry() should return a list of the items in the pantry', function(t){
   
-  setTimeout(function(){
-    var pantry = getPantry(household1._id);
-    console.log('This is what getPantry delivers: ', pantry);
-    t.ok(pantry.milk, 'Item listed.');
-    t.ok(pantry.fruit, 'Item listed.');
-    t.ok(pantry.chicken, 'All pantry items listed.');
-  }, 18000);
+  var pantry = getPantry(household1._id);
+  
+  t.ok(pantry.Milk, 'Item listed.');
+  t.ok(pantry.Berries, 'Item listed.');
+  t.ok(pantry.Chicken, 'All pantry items listed.');
 
   t.end();
 });
-*/
-test('getAppPantry() should retrieve the names of items in the app pantry', function(t){
-  
-  setTimeout(function(){
-    var appPantry = getAppPantry();
 
-    t.ok(appPantry.milk, 'App pantry list retrieved.');
-  }, 8000);
+test('getAppPantry() should retrieve the names of items in the app pantry', function(t){  
+  
+  var appPantry = getAppPantry();
+
+  t.ok(appPantry.Milk, 'App pantry list retrieved.');
 
   t.end();
 });
-/*
-test('removeFromPantry should remove items from the pantry', function(t){
-  
-  setTimeout(function(){
-    removeFromPantry('chicken', household1._id);
-  }, 23000);
 
-  setTimeout(function(){
+test('removeFromPantry() should remove items from the pantry', function(t){
+  
+  removeFromPantry('Chicken', household1._id)
+  .then(function(){
     Household.findOne({_id: household1._id }, 'pantry', function(err, household){
-      t.notOk('chicken' in household.pantry, 'Chicken removed from pantry.');
-      
+      t.notOk('Chicken' in household.pantry, 'Chicken removed from pantry.');
     });
-  }, 25000);
-
-  t.end();
+    
+    t.end();
+  });
 });
 
 test('buy() should move selected items from the current list to the pantry, fullyStocking them', function(t){
   
-  setTimeout(function(){
-    addToList('pie', household1._id);
-  }, 27000);
-
-  setTimeout(function(){
-    addToList('ice_cream', household1._id);
-  }, 30000);
-
-  setTimeout(function(){
-    buy(['ice_cream'], household1._id);
-  }, 33000);
-
-  setTimeout(function(){
+  
+  addToList('Durian', household1._id)
+  .then(function(){
+    addToList('Dessert', household1._id);
+  })
+  .then(function(){
+    buy(['Dessert'], household1._id);
+  })
+  .then(function(){
     Household.findOne({_id: household1._id }, 'pantry list', function(err, household){
-      
-      t.notOk('ice_cream' in household.list, 'Ice cream removed from list.');
-      t.equal(household.pantry['ice_cream'].fullyStocked, true, 'Ice cream in pantry.');
-      t.ok(household.list.pie, 'Pie still on list');
+      t.notOk('Dessert' in household.list, 'Dessert removed from list.');
+      t.equal(household.pantry['Dessert'].fullyStocked, true, 'Dessert in pantry.');
+      t.ok(household.list.Durian, 'Durian still on list');
     });
-  }, 35000);
-
-  t.end();
+    
+    t.end();
+  });
 });
 
-/*test('autoBuildList should add expired items automatically', function(t){
-  setTimeout(function(){addToPantry('carrots', household1._id, 7, 15);}, 200);
-  autoBuildList(household1._id);
-});*/
+test('autoBuildList() should add expired items automatically', function(t){
+  addToPantry('Roots', household1._id, 7, 15)
+  .then(function(){
+    autoBuildList(household1._id);
+  })
+  .then(function(){
+    Household.findOne({_id: household1._id }, 'pantry list', function(err, household){
+    //Test for presence in pantry and fullyStocked = false 
+      t.ok('Roots' in household.pantry, 'Roots added to pantry.');
+      t.notOk(household.pantry['Roots'].fullyStocked, 'Roots not fully stocked.');
 
-/*test('', function(t){t.end();});*/
-
-/*test('', function(t){t.end();});*/
-
-/*test('', function(t){t.end();});*/
-setTimeout(function(){
-  Household.findOneAndRemove();
-  mongoose.connection.db.dropCollection('households', function(err, result){
-    if(err){ console.error(err); }
-    mongoose.connection.close();
+      //Clear and close db connection
+      Household.findOneAndRemove();
+      mongoose.connection.db.dropCollection('households', function(err, result){
+        if(err){ console.error(err); }
+        mongoose.connection.close();
+      });
+      t.end();
+    });
   });
-}, 7000);
+});
