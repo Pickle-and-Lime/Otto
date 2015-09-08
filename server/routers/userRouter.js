18673859282975d4672093a0d3999788413cc8fa
Kamerynn Harrah <kamharrah8@gmail.com>
2015-09-08 11:25:09 -0400

    (feat) Add invitation functionality to User routes

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -6,10 +6,25 @@ var router = express.Router();
 /**
  *  GET /user/:id
  * 
- *  Todo: retrieve information about user, including outstanding invites
+ *  Retrieves information about user, including outstanding invites
+ *  
+ *  @return JSON
+ *          { 
+ *            userId:"abc", 
+ *            email:"test@example.com", 
+ *            householdId:"def", 
+ *            invites: ['bob@gmail.com', 'karen@gmail.com']
+ *          }
  */
 router.get('/:id', function(req, res) {
-  res.sendStatus(200);
+  var userId = req.params.id;
+  userHelpers.getUser(userId)
+  .then(function(user) {
+    res.send(user);
+  })
+  .catch(function() {
+    res.sendStatus(404);
+  })
 });
 
 /**
@@ -20,7 +35,7 @@ router.get('/:id', function(req, res) {
     and return householdId
  *
  *  Receives { "userId": "123abc", "email": "example@site.com"}
- *  Returns String householdId
+ *  @return String householdId
  */
 router.post('/', function(req, res) {
   var userId = req.body.userId;
@@ -30,9 +45,59 @@ router.post('/', function(req, res) {
     res.send(householdId);
   })
   .catch(function() {
-    res.status(404);
+    res.sendStatus(404);
   })
 });
+
+/**
+ *  POST /user/invite
+ *  
+ *  Creates an invitation for the user
+ * 
+ *  Invites a user, by email, to another user's household
+ *
+ *  E.g. John at john@gmail.com invites Kate at kate@gmail.com
+ *  Receives { "creatorEmail": "john@gmail.com", "inviteeEmail": "kate@gmail.com"}
+ *  @return 200 if successful, 404 if unsuccessful
+ */
+
+router.post('/invite', function(req, res) {
+  var creatorEmail = req.body.creatorEmail;
+  var inviteeEmail = req.body.inviteeEmail;
+  userHelpers.createInvitation(creatorEmail, inviteeEmail)
+  .then(function() {
+    res.sendStatus(200);
+  })
+  .catch(function() {
+    res.sendStatus(404);
+  })
+});
+
+/**
+ *  PUT /user/invite
+ *  
+ *  Updates user invitation
+ *
+ *  Either accepts or rejects pending invitation
+ *
+ *  E.g. Kate accepts John's invitation
+ *  Receives { creatorEmail: "john@gmail.com", inviteeEmail: "kate@gmail.com", accept: true }
+ *  @return Kate's new householdId if successful, 404 if unsuccessful
+ */
+
+router.put('/invite', function(req, res) {
+  var creatorEmail = req.body.creatorEmail;
+  var inviteeEmail = req.body.inviteeEmail;
+  var accept = req.body.accept;
+  userHelpers.updateInvitation(creatorEmail, inviteeEmail, accept)
+  .then(function(user) {
+    res.send(user.householdId);
+  })
+  .catch(function() {
+    res.sendStatus(404);
+  })
+});
+
 
 /**
  *  DELETE /user
@@ -43,3 +108,4 @@ router.delete('/', function(req, res) {
 });
 
 module.exports = router;
+ 
--- server/user-helpers.js
+++ server/user-helpers.js
@@ -1,3 +1,9 @@
+/**
+ * Provides methods for creating and updating Users
+ * @class userHelpers
+ * @static
+ */
+
 var mongoose = require('mongoose');
 var Household = require('./db/householdModel.js');
 var User = require('./db/userModel.js')
@@ -5,6 +11,37 @@ var Q = require('q');
 
 module.exports = userHelpers = {
 
+  /**
+   *  Retrieves and returns User information.
+   *
+   *  @method getUser
+   *  @param {string}     userId
+   *
+   *  @return {Promise}   Callback is supplied with User object.
+   */
+  getUser : function(userId) {
+    return User.findOne({ userId : userId})
+    .then(function(user) {
+      if (!user) throw new Error('Cannot find user');
+      return Q.fcall(function() {
+        return user;
+      })
+    })
+  },
+
+  /**
+   *  Retrieves and returns the householdId for a user.
+   *
+   *  If the user does not exist, it will create a new user with the
+   *  supplied userId and email. In this way, there is no separation
+   *  between signing in and signing up from the client's perspective.
+   *
+   *  @method getHouseholdForUser
+   *  @param  {string}    userId
+   *  @param  {string}    email
+   *
+   *  @return {Promise}   Callback is supplied with user's householdId.
+   */
   getHouseholdForUser : function(userId, email) {
     return User.findOne({ userId : userId})
     .then(function(user) {
@@ -25,6 +62,16 @@ module.exports = userHelpers = {
     })
   },
 
+  /**
+   *  Creates a new User and a new Household for that user.
+   *  The user is added to the households `users` property.
+   *
+   *  @method createUser
+   *  @param  {string}    userId
+   *  @param  {string}    email
+   *
+   *  @return {Promise}   Callback is supplied with new User object.
+   */
   createUser : function(userId, email) {
     // Create new household for the user
     var newHousehold = new Household({});
@@ -44,6 +91,65 @@ module.exports = userHelpers = {
           return newUser;
         })
       })
+    })
+  },
+
+  /**
+   *  Adds the creator's email into the invitee's `invites` property.
+   *  If invitation already exists, do nothing and return successfully.
+   *
+   *  @method createInvitation
+   *  @param  {string}    creatorEmail
+   *  @param  {string}    inviteeEmail
+   *
+   *  @return {Promise}   Callback is supplied with invitee's User object.
+  */
+  createInvitation : function(creatorEmail, inviteeEmail) {
+    return User.findOne({ email: inviteeEmail })
+    .then(function(user) {
+      if (!user) throw new Error('Cannot find user');
+      if (user.invites.indexOf(creatorEmail) === -1) {
+        user.invites.push(creatorEmail);
+      }
+      return user.save();
+    })
+  },
+
+  /**
+   *  Accepts or rejects the invitation.
+   *
+   *  Accepting will change the invitee's HouseholdId to
+   *  the creator's householdId.
+   *
+   *  Both accept and reject will remove the creator's email from
+   *  the `invites` property of the invitee's User DB entry.
+   *
+   *  @method updateInvitation
+   *  @param  {string}    creatorEmail
+   *  @param  {string}    inviteeEmail
+   *  @param  {boolean}   accept
+   *
+   *  @return {Promise}   Callback is supplied with invitee's User object
+   */
+  updateInvitation : function(creatorEmail, inviteeEmail, accept) {
+    if (typeof accept !== 'boolean') throw new Error('`Accept` must be a boolean');
+
+    return User.findOne({ email: inviteeEmail })
+    .then(function(invitee) {
+      if (!invitee) throw new Error('Cannot find invitee');
+
+      // Pull removes the element from the `invites` MongoDB Array, if present
+      invitee.invites.pull(creatorEmail);
+
+      if (accept) {
+        return User.findOne({ email: creatorEmail })
+        .then(function(creator) {
+          invitee.householdId = creator.householdId;
+          return invitee.save();
+        })
+      } else {
+        return invitee.save();
+      }
     })
   }
 }
