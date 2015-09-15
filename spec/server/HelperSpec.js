b7a5fccd5f148dd40f6188815b7849d308cf8b8e
Hailey Foster <haileyhasmail@gmail.com>
2015-09-15 11:09:07 -0700

    Add userTags property to items in the shopping list.

--- spec/server/HelperSpec.js
+++ spec/server/HelperSpec.js
@@ -112,6 +112,8 @@ test('addTag() should add a tag to an item in household\'s pantry and list', fun
     .then(function(){
       Household.findOne({_id: household1._id }, 'pantry list', function(err, household){
         t.ok(household.list.Chicken.tags.indexOf('Shredded') !== -1, 'Tag added to item in list');
+        t.ok(household.list.Chicken.userTags.indexOf('Shredded') !== -1, 'Tage added to userTags in list');
+        console.log(household.list);
         t.ok(household.pantry.Chicken.tags.indexOf('Shredded') !== -1, 'Tag added to item in pantry');
         t.end();
       });
@@ -126,6 +128,7 @@ test('addTag() should add a tag to an item not in the appPantry in household\'s 
     .then(function(){
       Household.findOne({_id: household1._id }, 'pantry list', function(err, household){
         t.ok(household.list.Guava.tags.indexOf('Whole') !== -1, 'Tag added to item in list');
+        t.ok(household.list.Guava.userTags.indexOf('Whole') !== -1, 'Tage added to userTags in list');
         t.ok(household.pantry.Guava.tags.indexOf('Whole') !== -1, 'Tag added to item in pantry');
         t.end();
       });
@@ -188,17 +191,15 @@ test('buy() should move selected items from the current list to the pantry, full
   
   addToList('Durian', household1._id)
   .then(function(){
-    addToList('Dessert', household1._id)
+    buy(['Guava'], household1._id)
     .then(function(){
-      buy(['Dessert'], household1._id)
-      .then(function(){
-        Household.findOne({_id: household1._id }, 'pantry list', function(err, household){
-          t.notOk('Dessert' in household.list, 'Dessert removed from list.');
-          t.equal(household.pantry.Dessert.fullyStocked, true, 'Dessert in pantry.');
-          t.ok(household.list.Durian, 'Durian still on list');
-          t.end();
-        });  
-      });
+      Household.findOne({_id: household1._id }, 'pantry list', function(err, household){
+        console.log('LISTSTST', household.list);
+        t.notOk('Guava' in household.list, 'Guava removed from list.');
+        t.equal(household.pantry.Guava.fullyStocked, true, 'Guava in pantry.');
+        t.ok(household.list.Durian, 'Durian still on list');
+        t.end();
+      });  
     });
   });
 });
