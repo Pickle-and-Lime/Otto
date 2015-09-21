8b90136743d7fcb59eac54ec6836b047ed57b25c
Hailey Foster <haileyhasmail@gmail.com>
2015-09-20 17:01:53 -0700

    Update docs

---
+++ api.js
@@ -0,0 +1,102 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "PantryItem",
+        "app",
+        "db",
+        "householdController",
+        "householdCtrl",
+        "householdModel",
+        "itemCtrl",
+        "listCtrl",
+        "loginController",
+        "pantryCtrl",
+        "userCtrl",
+        "userModel",
+        "utils"
+    ],
+    "modules": [
+        "Household Controller",
+        "Login Controller",
+        "PantryItem",
+        "app",
+        "db",
+        "householdCtrl",
+        "householdModel",
+        "itemCtrl",
+        "listCtrl",
+        "pantryCtrl",
+        "userCtrl",
+        "userModel",
+        "utils"
+    ],
+    "allModules": [
+        {
+            "displayName": "app",
+            "name": "app",
+            "description": "configures server routes"
+        },
+        {
+            "displayName": "db",
+            "name": "db",
+            "description": "Create mongod connection using mongoose"
+        },
+        {
+            "displayName": "Household Controller",
+            "name": "Household Controller",
+            "description": "Household Controller: Controlls household.html and createHousehold.html"
+        },
+        {
+            "displayName": "householdCtrl",
+            "name": "householdCtrl",
+            "description": "Provides methods for working with households"
+        },
+        {
+            "displayName": "householdModel",
+            "name": "householdModel",
+            "description": "Creates a mongoose model and mongo schema for households"
+        },
+        {
+            "displayName": "itemCtrl",
+            "name": "itemCtrl",
+            "description": "Provides methods for creating household pantries and lists"
+        },
+        {
+            "displayName": "listCtrl",
+            "name": "listCtrl",
+            "description": "Provides methods for creating household pantries and lists"
+        },
+        {
+            "displayName": "Login Controller",
+            "name": "Login Controller",
+            "description": "Login Controller: Controlls login.html, account.html, zip.html"
+        },
+        {
+            "displayName": "pantryCtrl",
+            "name": "pantryCtrl",
+            "description": "Provides methods for creating household pantries and lists"
+        },
+        {
+            "displayName": "PantryItem",
+            "name": "PantryItem",
+            "description": "This module sets up individual items in the general pantry\nIt creates a neural network for each item and sets default values for\nthe trainingSet and average time to expiration for each item.\nIt also creates methods for creating and updating user-specific pantry items."
+        },
+        {
+            "displayName": "userCtrl",
+            "name": "userCtrl",
+            "description": "Provides methods for creating and updating Users"
+        },
+        {
+            "displayName": "userModel",
+            "name": "userModel",
+            "description": "Creates a mongoose model and mongo schema for users"
+        },
+        {
+            "displayName": "utils",
+            "name": "utils",
+            "description": "Provides general utility methods"
+        }
+    ],
+    "elements": []
+} };
+});
